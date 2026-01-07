e18762af9aafb5d1a60504a3e8caf57386ed04d6
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2026-01-07 17:11:52 +0500

    Handle empty country data response

--- src/checkoutItems/BillingDetails.js
+++ src/checkoutItems/BillingDetails.js
@@ -9,7 +9,7 @@ function BillingDetails({ data, setdata }) {
             try {
                 const response = await fetch('https://restcountries.com/v3.1/all');
                 const data = await response?.json();
-                setCountries(data);
+                setCountries(data || []);
             } catch (error) {
                 console.error('Error fetching country data:', error);
             }
